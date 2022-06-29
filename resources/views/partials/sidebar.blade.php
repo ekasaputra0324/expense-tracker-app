4999c218cfe3eb83b7314e423fc7691fa7a73658
ekasaputra0324 <paijomr4221@gmail.com>
2022-06-29 01:33:34 -0700

    configuration template

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+    <div class="logo">
+      <a href="https://www.creative-tim.com" class="simple-text logo-mini">
+        <div class="logo-image-small">
+         <img src="../assets/img/rename.png" style="width: 20px; margin-top:-5px;">
+        </div>
+        <!-- <p>CT</p> -->
+      </a>
+      <a href="https://www.creative-tim.com" class="logo-normal" style="color: black; text-decoration:none;">
+        Exepens Tracker
+        <!-- <div class="logo-image-big">
+          <img src="../assets/img/logo-big.png">
+        </div> -->
+      </a>
+    </div>
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="active ">
+          <a href="./dashboard.html">
+            <i class="fa-solid fa-house-chimney"></i>
+            <p >Dashboard</p>
+          </a>
+        </li>
+        <li>
+          <a href="./icons.html">
+            <i class="fa-solid fa-calendar"></i>
+            <p>Mutations</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
